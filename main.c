7db4ddd5046d4523b53c90de451ff6108079611a
Will von Behren <willvb3@hotmail.com>
2017-01-19 16:22:59 -0500

    Add ability to read input file character-by-character

--- main.c
+++ main.c
@@ -1,28 +1,44 @@
+#define BUFF_SIZE 255
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char ** argv)
 {
-   FILE *ifp;				/* input file pointer */
+   FILE * ifp;																	/* input file pointer */
+   char read_buff[BUFF_SIZE];												/* input buffer */
+   int count;																	/* counter */
 
-   if(argc != 2)			/*error check for file arguments*/
+   if(argc != 2)																/* error check for file arguments */
    {	
-	fprintf(stderr, "usage: name file\n"); 
-	exit(1);
+      fprintf(stderr, "usage: name file\n"); 
+      exit(1);
    }
 
-   ifp = fopen(argv[1], "r");		/*open file*/
-   if(ifp == NULL)			/*error check for opening file*/
+   ifp = fopen(argv[1], "r");												/* open file */
+   if(ifp == NULL)															/* error check for opening file */
    {
-	fprintf(stderr, "error: file does not exist\n");
-	exit(1);
+      fprintf(stderr, "error: file does not exist\n");			/* error check for nonexistent file */
+      exit(1);
    }
+
+   count = 0;
+   read_buff[count] = fgetc(ifp);										/* new character */
+	while(read_buff[count] == ' '  ||									/* clear white-space */
+			read_buff[count] == '\n' || 
+			read_buff[count] == '\t')											
+		read_buff[count] = fgetc(ifp);																		
+	
+   while(read_buff[count] != EOF)										/* read char one-by-one from input file into read_buff */
+   {
+   	count++;
+   	read_buff[count] = fgetc(ifp);
+	}
+
+	read_buff[count] = '\0';
+   
+   fprintf(stdout, "%s", read_buff);
 
    return 0;
 }
-
-
-
-
 
 
